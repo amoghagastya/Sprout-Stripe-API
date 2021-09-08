724683d1295c4f98c48b6bb17ab352125bd186bc
Amogh Agastya <amagastya@gmail.com>
2021-09-08 22:25:24 +0530

    Added try/catch

--- src/api.js
+++ src/api.js
@@ -16,38 +16,44 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const YOUR_DOMAIN = 'http://sprout.ae';
 
 
-
-router.post('/stripe-checkout', async (req, res) => {
-  const total = req.body.total;
-  const name = req.body.name;
-  const email = req.body.email;
-  const user_cart = req.body.cart;
-  // const woo_id = req.body.id;
-  var session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        // TODO: replace this with the `price` of the product you want to sell
-        amount : total,
-        currency : "inr",
-        name : "" + name + "'s Sprout Order",
-        quantity : 1,
-        description : "Payment for cart items " + user_cart
-      },
-    ],
-    payment_method_types: [
-      'card',
-    ],
-    mode: 'payment',
-    success_url: `https://localhost:9000`,
-    cancel_url: `https://sprout.ae`,
-    customer_email: email
+try {
+  router.post('/stripe-checkout', async (req, res) => {
+    const total = req.body.total;
+    const name = req.body.name;
+    const email = req.body.email;
+    const user_cart = req.body.cart;
+    // const woo_id = req.body.id;
+    var session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          // TODO: replace this with the `price` of the product you want to sell
+          amount : total,
+          currency : "inr",
+          name : "" + name + "'s Sprout Order",
+          quantity : 1,
+          description : "Payment for cart items " + user_cart
+        },
+      ],
+      payment_method_types: [
+        'card',
+      ],
+      mode: 'payment',
+      success_url: `https://localhost:9000`,
+      cancel_url: `https://sprout.ae`,
+      customer_email: email
+    });
+  
+    // session["woo_id"] = woo_id;
+    // res.redirect(303, session.url)
+    console.log(session)
+    return res.status(200).send(session)
   });
+  
 
-  // session["woo_id"] = woo_id;
-  // res.redirect(303, session.url)
-  console.log(session)
-  return res.status(200).send(session)
-});
+}
+catch(error) {
+  alert(error)
+}
 
 
 app.use('/.netlify/functions/api', router);
